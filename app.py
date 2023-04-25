6efe20d385c615ad7fac39c210e10fb8f14ad856
kangCoder <khsofficial1213@gmail.com>
2023-04-25 13:36:00 +0900

    Feat: text extract logic 수정. Soup -> goose

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, jsonify
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-import sys
+from goose3 import Goose
+
+g = Goose({'browser_user_agent': 'Mozilla', 'parser_class': 'lxml'})
+
 
 # __name__ => 모듈명. 여기서는 app.py 라는 모듈이 실행 되므로 main 이라는 문자열이 담긴다.
 app = Flask(__name__)
@@ -25,19 +26,6 @@ def textExtract():
 
 
 def extract(url):
-    html = urlopen(url).read()
-    soup = BeautifulSoup(html, features="html.parser")
-
-    for script in soup(["script", "style"]):
-        script.extract()
-
-    text = soup.get_text()
-
-    # break into lines and remove leading and trailing space on each
-    lines = (line.strip() for line in text.splitlines())
-    # break multi-headlines into a line each
-    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-    # drop blank lines
-    text = '\n'.join(chunk for chunk in chunks if chunk)
-
+    article = g.extract(url=url)
+    text = article.cleaned_text
     return text
